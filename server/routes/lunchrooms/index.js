4ede83aa8968788a9e94b088c5e4a104d977a97c
Christoph Dorn <christoph@christophdorn.com>
2015-08-14 10:17:10 -0700

    sitemap for implemented uris

--- server/routes/lunchrooms/index.js
+++ server/routes/lunchrooms/index.js
@@ -1,7 +1,21 @@
 var router = module.exports = require('express').Router();
 
 router.get( '/', function( req, res ){
-  res.send('Hello, lunchroom world!');
+
+  res.send("<ul>" + [
+  	'/landing',
+  	'/admin/kitchen-sink',
+  	'/pages/menu',
+  	'/pages/login',
+  	'/pages/register',
+  	'/pages/checkout',
+  	'/emails/menu',
+  	'/emails/lunch-arrival',
+  	'/emails/receipt'
+  ].map(function (uri) {
+  	return '<li><a href="' + uri + '">' + uri + '</a></li>';
+  }).join("\n") + "</ul>");
+
 });
 
 router.get( '/:id', function( req, res ){
