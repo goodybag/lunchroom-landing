e91d1ff9c5e696b96ecabb832e64eae6ba546731
John Fawcett <jrf0110@gmail.com>
2015-08-14 15:13:09 -0500

    new js lib target, add in cc formatting

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,5 @@
-window.$ = window.jQuery = require('jquery');
-
-require('jquery-popover');
-require('bootstrap/js/modal');
-require('bootstrap/js/dropdown');
-require('utils');
-
 $(function(){
   $('[data-role="popover"]').popover();
+  $('[name="card_number"]').payment('formatCardNumber');
+  $('[name="card_cvv"]').payment('formatCardCVC');
 });
