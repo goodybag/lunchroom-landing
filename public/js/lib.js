60adcea82b42f23524e658ef5c3fbbc693dbcf89
John Fawcett <jrf0110@gmail.com>
2015-08-14 16:20:16 -0500

    add jquery phone number thingY
    try to eliminate invalid nums
    
    remove useless prop

--- public/js/lib.js
+++ public/js/lib.js
@@ -2,6 +2,7 @@ window.$ = window.jQuery = require('jquery');
 
 require('jquery.payment');
 require('jquery-popover');
+require('intl-tel-input');
 require('bootstrap/js/modal');
 require('bootstrap/js/dropdown');
 require('utils');
