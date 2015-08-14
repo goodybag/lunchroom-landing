711d5fe00e88b31f5f828dd4ac67896dcf970bf4
John Fawcett <jrf0110@gmail.com>
2015-08-14 15:25:07 -0500

    add component pragmas

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,7 @@
 $(function(){
   $('[data-role="popover"]').popover();
+  // @component:id checkout-form
   $('[name="card_number"]').payment('formatCardNumber');
+  // @component:id checkout-form
   $('[name="card_cvv"]').payment('formatCardCVC');
 });
