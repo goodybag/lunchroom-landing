edc6ad668100608b186921f6d682604d47d3c8bd
John Fawcett <jrf0110@gmail.com>
2015-08-16 21:25:01 -0500

    use photo_url for item photo

--- server/data/dummy.js
+++ server/data/dummy.js
@@ -2,49 +2,49 @@ module.exports.items = [
   { name: 'Pirata Dos Tacos'
   , price: 1090
   , dietTags: ['gluten-free', 'spicy']
-  , coverPhotoUrl: 'https://www.filepicker.io/api/file/SZoK9zUvTPWXzmK81aGg/convert?w=400&h=195&fit=crop'
+  , photo_url: 'https://www.filepicker.io/api/file/SZoK9zUvTPWXzmK81aGg'
   }
 , { name: 'Poop Taco Box'
   , price: 1200
   , quantity: 2
   , dietTags: ['vegan', 'vegetarian']
-  , coverPhotoUrl: 'https://www.filepicker.io/api/file/SZoK9zUvTPWXzmK81aGg/convert?w=400&h=195&fit=crop'
+  , photo_url: 'https://www.filepicker.io/api/file/SZoK9zUvTPWXzmK81aGg'
   }
 , { name: 'Pirata Dos Tacos'
   , price: 1090
   , quantity: 1
   , dietTags: []
-  , coverPhotoUrl: 'https://www.filepicker.io/api/file/SZoK9zUvTPWXzmK81aGg/convert?w=400&h=195&fit=crop'
+  , photo_url: 'https://www.filepicker.io/api/file/SZoK9zUvTPWXzmK81aGg'
   }
 , { name: 'Poop Taco Box but this one is a really long title'
   , price: 1200
   , dietTags: ['gluten-free', 'spicy']
-  , coverPhotoUrl: 'https://www.filepicker.io/api/file/SZoK9zUvTPWXzmK81aGg/convert?w=400&h=195&fit=crop'
+  , photo_url: 'https://www.filepicker.io/api/file/SZoK9zUvTPWXzmK81aGg'
   }
 , { name: 'Poop Taco Box'
   , price: 1200
   , dietTags: ['gluten-free', 'spicy']
-  , coverPhotoUrl: 'https://www.filepicker.io/api/file/SZoK9zUvTPWXzmK81aGg/convert?w=400&h=195&fit=crop'
+  , photo_url: 'https://www.filepicker.io/api/file/SZoK9zUvTPWXzmK81aGg'
   }
 , { name: 'Pirata Dos Tacos'
   , price: 1090
   , dietTags: ['gluten-free', 'spicy']
-  , coverPhotoUrl: 'https://www.filepicker.io/api/file/SZoK9zUvTPWXzmK81aGg/convert?w=400&h=195&fit=crop'
+  , photo_url: 'https://www.filepicker.io/api/file/SZoK9zUvTPWXzmK81aGg'
   }
 , { name: 'Poop Taco Box'
   , price: 1200
   , dietTags: ['gluten-free', 'spicy']
-  , coverPhotoUrl: 'https://www.filepicker.io/api/file/SZoK9zUvTPWXzmK81aGg/convert?w=400&h=195&fit=crop'
+  , photo_url: 'https://www.filepicker.io/api/file/SZoK9zUvTPWXzmK81aGg'
   }
 , { name: 'Poop Taco Box'
   , price: 1200
   , dietTags: ['gluten-free', 'spicy']
-  , coverPhotoUrl: 'https://www.filepicker.io/api/file/SZoK9zUvTPWXzmK81aGg/convert?w=400&h=195&fit=crop'
+  , photo_url: 'https://www.filepicker.io/api/file/SZoK9zUvTPWXzmK81aGg'
   }
 , { name: 'Pirata Dos Tacos'
   , price: 1090
   , dietTags: ['gluten-free', 'spicy']
-  , coverPhotoUrl: 'https://www.filepicker.io/api/file/SZoK9zUvTPWXzmK81aGg/convert?w=400&h=195&fit=crop'
+  , photo_url: 'https://www.filepicker.io/api/file/SZoK9zUvTPWXzmK81aGg'
   }
 ];
 
@@ -52,13 +52,13 @@ module.exports.order = {
   items:  [ { name: 'Pirata Dos Tacos'
             , price: 1090
             , dietTags: ['gluten-free', 'spicy']
-            , coverPhotoUrl: 'https://www.filepicker.io/api/file/SZoK9zUvTPWXzmK81aGg/convert?w=352&h=210&fit=crop'
+            , photo_url: 'https://www.filepicker.io/api/file/SZoK9zUvTPWXzmK81aGg/convert?w=352&h=210&fit=crop'
             , quantity: 1
             }
           , { name: 'Poop Taco Box'
             , price: 1200
             , dietTags: ['vegan', 'vegetarian']
-            , coverPhotoUrl: 'https://www.filepicker.io/api/file/SZoK9zUvTPWXzmK81aGg/convert?w=352&h=210&fit=crop'
+            , photo_url: 'https://www.filepicker.io/api/file/SZoK9zUvTPWXzmK81aGg/convert?w=352&h=210&fit=crop'
             , quantity: 1
             }
           ]
