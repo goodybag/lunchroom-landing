a05c6de0ea8d0352238218158a69cddcccef5d7c
Christoph Dorn <christoph@christophdorn.com>
2015-08-26 12:51:24 -0700

    integrate validation properly; runs same for skin dev and now also allows for nicely exported methods a consumer can call to interact with skin

--- public/js/app.js
+++ public/js/app.js
@@ -1,51 +1,102 @@
 $(function(){
 
-  function attachSkinApp (config) {
+  function attachSkinApp (config, apiConsumer) {
     config = config || {};
+    if (typeof config.initGeneric === "undefined") {
+      config.initGeneric = true;
+    }
+    if (typeof config.initSkinDev === "undefined") {
+      config.initSkinDev = true;
+    }
+
     config.intlTelUtilsScriptPath = config.intlTelUtilsScriptPath || "/dist/utils.js";
 
-    $('[data-role="popover"]').popover();
-    // @component:id checkout-form
-    $('[name="card_number"]').payment('formatCardNumber');
-    // @component:id checkout-form
-    $('[name="card_cvv"]').payment('formatCardCVC');
-    // @component:id checkout-form
-    $('[name="phone"]').intlTelInput({
-      autoFormat: true
-    , utilsScript: config.intlTelUtilsScriptPath
-    , preventInvalidNumbers: true
-    });
 
-    var checkoutValidator = validators.createCheckoutValidator(
-      $('#section-checkout-info-validation-test .checkout-info')
-    );
+    function initGeneric (consumerApi, withinElement) {
 
-    $('.btn-validate').click( function( e ){
-      checkoutValidator.validate();
-    });
+      // Generic helpers that can always attach to any matched elements.
 
-    $('#section-checkout-info-validation-test input').blur( function( e ){
-      if ( ['checkbox', 'radio'].indexOf( e.target.type ) > -1 ) return;
-
-      if ( e.target.name === 'card_expiration_month' ) return;
-      if ( e.target.name === 'card_expiration_year' ){
-        return checkoutValidator.validate('card_expiration');
-      }
-
-      checkoutValidator.validate( e.target.name );
-    });
-
-    $('.checkout-info').each( function(){
-      var $el = $(this);
-
-      $el.find('[name="will_add_new_card"]').change( function( e ){
-        var $wrapper = $el.find('.payment-method-wrapper');
-        $wrapper.toggleClass( 'disabled', !e.target.checked );
-        $wrapper.find('input').attr( 'disabled', !e.target.checked ? 'disabled' : null );
+      $('[data-role="popover"]', withinElement).popover();
+      // @component:id checkout-form
+      $('[name="card_number"]', withinElement).payment('formatCardNumber');
+      // @component:id checkout-form
+      $('[name="card_cvv"]', withinElement).payment('formatCardCVC');
+      // @component:id checkout-form
+      $('[name="phone"]', withinElement).intlTelInput({
+        autoFormat: true
+      , utilsScript: config.intlTelUtilsScriptPath
+      , preventInvalidNumbers: true
       });
-    });
 
-    $('[data-toggle="tooltip"]').tooltip();
+      $('.checkout-info', withinElement).each( function(){
+        var $el = $(this);
+
+        $el.find('[name="will_add_new_card"]').change( function( e ){
+          var $wrapper = $el.find('.payment-method-wrapper');
+          $wrapper.toggleClass( 'disabled', !e.target.checked );
+          $wrapper.find('input').attr( 'disabled', !e.target.checked ? 'disabled' : null );
+        });
+      });
+
+      $('[data-toggle="tooltip"]', withinElement).tooltip();
+
+    }
+
+    function initSkinDev (consumerApi) {
+
+      // Helpers to animate skin for development and showcase how
+      // to use APIs to interact with skin.
+
+      var checkoutValidator = consumerApi.validators.createCheckoutValidator(
+        $('#section-checkout-info-validation-test .checkout-info')
+      );
+
+      $('.btn-validate').click( function( e ){
+        checkoutValidator.validate();
+      });
+
+      consumerApi.monitors.createCheckoutMonitor(
+        $('#section-checkout-info-validation-test')
+      );
+    }
+
+
+    var consumerApi = {
+      validators: validators,
+      monitors: {
+        createCheckoutMonitor: function (formElement) {
+
+          var checkoutValidator = consumerApi.validators.createCheckoutValidator(
+            $('.checkout-info', formElement)
+          );
+
+          $('input', formElement).blur( function( e ){
+            if ( ['checkbox', 'radio'].indexOf( e.target.type ) > -1 ) return;
+
+            if ( e.target.name === 'card_expiration_month' ) return;
+            if ( e.target.name === 'card_expiration_year' ){
+              return checkoutValidator.validate('card_expiration');
+            }
+
+            checkoutValidator.validate( e.target.name );
+          });
+
+          initGeneric(consumerApi, formElement);
+        }
+      }
+    };
+
+
+    if (config.initGeneric) {
+      initGeneric(consumerApi, $('body'));
+    }
+    if (config.initSkinDev) {
+      initSkinDev(consumerApi);
+    }
+
+    if (apiConsumer) {
+      apiConsumer(consumerApi);
+    }
   }
 
   // Allow app to skip auto-attachment on load by setting
